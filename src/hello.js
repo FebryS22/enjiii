668d8ec3493c895aba0f203d7b60127fea375380
FebryS22 <febrysirape@gmail.com>
2023-03-07 10:11:08 +0700

    pertemuan 2 tugas react

---
+++ src/hello.js
@@ -0,0 +1,16 @@
+
+
+function hello() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <h1>hallo world</h1>
+          
+        </header>
+      </div>
+    );
+  }
+  
+  export default hello;
+  
